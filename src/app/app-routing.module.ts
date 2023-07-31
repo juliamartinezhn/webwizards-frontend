36ab09693368aa26b1b865c20f41b64c6f9d3d88
sramador45 <58308854+sramador45@users.noreply.github.com>
2023-07-30 19:03:41 -0600

    Commit2
    Creacion de componentes sign up, sign in, planes ,  footer y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {path: 'sign-up', component: SignupComponent},
+{path:'log-in', component:LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
