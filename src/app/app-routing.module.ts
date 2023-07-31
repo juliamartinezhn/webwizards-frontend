b4ed502a61f7de5356b93e85c34f760393aca3ad
juliamartinezhn <102008430+juliamartinezhn@users.noreply.github.com>
2023-07-30 20:49:00 -0600

    AMS-2 Modificaciones registro e inicio de sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [ {path: 'sign-up', component: SignupComponent},
-{path:'log-in', component:LoginComponent}];
+const routes: Routes = [
+  {
+    path: 'registro',
+    component: SignupComponent
+  },
+  {
+    path: 'inicio-sesion',
+    component: LoginComponent
+  },
+  {
+    path:'dashboard',
+    component:DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
