42a21d573ca95c4f2d250cbee426cb257e1f26d4
Julia Martínez <102008430+juliamartinezhn@users.noreply.github.com>
2023-08-14 01:42:23 -0600

    Revert "Commit inconcluso error span"

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -5,42 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-
-export class SignupComponent implements OnInit {
-  formularioRegistro = new FormGroup({
-    nombre:new FormControl('', [Validators.required,Validators.maxLength(10)]),
-    apellido:new FormControl('',[Validators.required,Validators.maxLength(10)]),
-    email: new FormControl ('',[Validators.required, Validators.pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)]),
-    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/)]),
-    fechaNacimiento:new FormControl('',[Validators.required])
-   
-  });
-
-  constructor(){  }
-  ngOnInit () : void {
-  
-  }
-  get nombre (){
-    return this.formularioRegistro.get('nombre');
-  }
-  get apellido (){
-    return this.formularioRegistro.get('apellido');
-  }
-  get email (){
-    return this.formularioRegistro.get('email');
-  }
-  get password (){
-    return this.formularioRegistro.get('password');
-  }
-
-  get fechaNacimiento (){
-    return this.formularioRegistro.get('fechaNacimiento');
-  }
-  iniciarSesion(){
-    console.log(this.formularioRegistro.value);
-    console.log(this.formularioRegistro.value);
-
-  }
-
+export class SignupComponent {
 
 }
